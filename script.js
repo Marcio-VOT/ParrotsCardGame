fa77b26b50bb6782d745834945b7be63093eb005
MarcioJrPC <marciofsantos65@gmail.com>
2022-10-22 20:11:44 -0300

    serteia e coloca cartas

--- script.js
+++ script.js
@@ -2,6 +2,11 @@ let ncartas = 0;
 const fundos = [ "bobross", "explody", "fiesta", "metal", "revertit", "triplets", "unicorn"]
 const fundosTotal = [];
 const container = document.querySelector(".cartas")
+let click = 1;
+let seguraCarta1
+let seguraCarta2
+let contaAcertos = 0
+let jogadas = 0;
 
 while(ncartas%2 != 0 || ncartas > 14 || ncartas < 4){
     ncartas = Number(prompt("quantas cartas você quer?"));
@@ -12,9 +17,15 @@ for(let i = 0; i<ncartas/2; i++){
     fundosTotal.push(fundos[i]);
 }
 
+function comparador() { 
+    return Math.random() - 0.5; 
+}
+fundosTotal.sort(comparador);
+// cria cartas --------------------------------------
 for(let i = 0; i < ncartas ; i++){
+
     const ct = `
-    <div class="carta">
+    <div id="${fundosTotal[i]}" class="carta" onclick="marcaCarta(this)">
                 <div class="front-face face">
                     <img src="back.png" >
                 </div>
@@ -25,3 +36,5 @@ for(let i = 0; i < ncartas ; i++){
     `
     container.innerHTML += ct;
 }
+// cria cartas --------------------------------------
+
